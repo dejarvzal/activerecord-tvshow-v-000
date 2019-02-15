588b1fa126013ac4ae2b1239755cb35d47eefcac
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2019-02-15 05:04:07 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -10,4 +10,13 @@ class Show < ActiveRecord::Base
     end
 
 
+    def self.lowest_rating
+      self.minimum(:rating)
+    end
+
+    def self.least_popular_show
+      self.where("rating = ?", self.lowest_rating).first
+    end
+
+
 end
